49a6a8ac289f54d6cdf90a242d787efab763d539
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-22 12:30:48 -0500

    feat: add navlink to link nav with pages

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <div>
-            <h1>navigation</h1>
-        </div>
+        <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
+            <Navbar.Brand>
+                Task Manager
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link as={NavLink} to="/projects">Projectos</Nav.Link>
+                    <NavDropdown title="Admin">
+                        <NavDropdown.Item as={NavLink} to="/admin/users">Usuarios</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Nav>
+                    <Nav.Link as={NavLink} to="/login">Iniciar Sesión</Nav.Link>
+                    <Nav.Link as={NavLink} to="/register">Registrarse</Nav.Link>
+                    <Nav.Link as={NavLink} to="/account">Mi Cuenta</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+
+        </Navbar>
     );
 };
 
