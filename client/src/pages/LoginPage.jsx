68b86602c1351e327dcb37a3f193bf70863c61e8
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-25 16:03:53 -0500

    add: login button

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import AuthContext from '../contexts/AuthContext';
+
+const userCredentials = {};
 
 const LoginPage = () => {
+
+    const {login} = useContext(AuthContext);
+    
     return (
         <div>
             <h1>Login Page</h1>
+            <button onClick={() => login(userCredentials)}>Iniciar Sesión</button>
         </div>
     );
 };
