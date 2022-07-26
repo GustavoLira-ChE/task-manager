ac391929fe6a3e0f798cba3fc9b378efcde718df
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-26 15:40:16 -0500

    fix: add Layout1 and Router for using React-router-dom into entire app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import { AuthProvider } from "./contexts/AuthContext";
 import AppRouter from "./routers/AppRouter";
+import { BrowserRouter as Router } from "react-router-dom";
+import Layout1 from "./components/Layouts/Layout1";
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider>
-        <AppRouter />
-      </AuthProvider>
+      <Router>
+        <AuthProvider>
+          <Layout1>
+            <AppRouter />
+          </Layout1>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
