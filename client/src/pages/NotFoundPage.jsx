de5ceae3ecbb6175abe92a0a27ac54df1ebc6022
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-26 16:34:17 -0500

    style: add format to Not found Page

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import routes from '../helpers/routes';
+import { Link } from "react-router-dom"
 
 const NotFoundPage = () => {
     return (
-        <div>
-            <h1>Not Found Page</h1>
-        </div>
+        <Container>
+            <Row style={{marginTop: "2rem"}}>
+                <Col className='text-center' md={{span: 6, offset: 3}}>
+                    <img
+                    className='w-100' 
+                    src="/img/404-not-found.svg" 
+                    alt="error-404" />
+                    <h2>¿Te has perdido?</h2>
+                    <p>Vuelve al <Link to={routes.home}>inicio</Link> </p>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
