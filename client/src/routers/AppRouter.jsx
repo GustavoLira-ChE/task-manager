f0fb426054c970c8224324658c65b68016b8b265
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-26 15:46:32 -0500

    fix: remove layout1 and Router components for using in App.js

--- client/src/routers/AppRouter.jsx
+++ client/src/routers/AppRouter.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout1 from "../components/Layouts/Layout1";
+import { Routes, Route } from "react-router-dom";
 import roles from "../helpers/roles";
 import routes from "../helpers/routes";
 import AccountPage from "../pages/AccountPage";
@@ -17,20 +16,16 @@ import PublicRoute from "./PublicRoute";
 const AppRouter = () => {
     return (
         <div>
-            <Router>
-                <Layout1>
-                    <Routes>
-                        <Route exact path={routes.home} element={<PublicRoute><HomePage /></PublicRoute>} />
-                        <Route exact path={routes.login} element={<PublicRoute><LoginPage /></PublicRoute>} />
-                        <Route exact path={routes.register} element={<PublicRoute><RegisterPage /></PublicRoute>} />
-                        <Route exact path={routes.account} element={<PrivateRoute><AccountPage /></PrivateRoute>} />
-                        <Route exact path={routes.projects} element={<PrivateRoute><ProjectsPage /></PrivateRoute>} />
-                        <Route exact path={routes.project()} element={<PrivateRoute><ProjectPage /></PrivateRoute>} />
-                        <Route exact path={routes.admin.users} element={<PrivateRoute hasRole={roles.admin}><UsersPage /></PrivateRoute>} />
-                        <Route exact path="*" element={<NotFoundPage />} />
-                    </Routes>
-                </Layout1>
-            </Router>
+            <Routes>
+                <Route exact path={routes.home} element={<PublicRoute><HomePage /></PublicRoute>} />
+                <Route exact path={routes.login} element={<PublicRoute><LoginPage /></PublicRoute>} />
+                <Route exact path={routes.register} element={<PublicRoute><RegisterPage /></PublicRoute>} />
+                <Route exact path={routes.account} element={<PrivateRoute><AccountPage /></PrivateRoute>} />
+                <Route exact path={routes.projects} element={<PrivateRoute><ProjectsPage /></PrivateRoute>} />
+                <Route exact path={routes.project()} element={<PrivateRoute><ProjectPage /></PrivateRoute>} />
+                <Route exact path={routes.admin.users} element={<PrivateRoute hasRole={roles.admin}><UsersPage /></PrivateRoute>} />
+                <Route exact path="*" element={<NotFoundPage />} />
+            </Routes>
         </div>
     );
 };
