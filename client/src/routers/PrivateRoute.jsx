c3fc02cf08ad7f6bfd0b06038e62e4628fa7b30d
Gustavo Lira <gu.lirar23@gmail.com>
2022-07-26 15:45:41 -0500

    feat: add useLocation to pass previous page pathname to login page

--- client/src/routers/PrivateRoute.jsx
+++ client/src/routers/PrivateRoute.jsx
@@ -1,14 +1,15 @@
 import React, {useContext} from 'react';
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import AuthContext from '../contexts/AuthContext';
 import routes from '../helpers/routes';
 
-const PrivateRoute = ( {hasRole: role ,children} ) => {
+const PrivateRoute = ( {hasRole: role, children} ) => {
 
     const { hasRole, isLogged} = useContext(AuthContext);
+    const location = useLocation();
 
-    if(hasRole(role)) return <Navigate to={routes.projects} /> 
-    if(!isLogged()) return <Navigate to={routes.login} />
+    if(hasRole(role) && isLogged()) return <Navigate to={routes.projects} /> 
+    if(!isLogged()) return <Navigate to={routes.login} state={{from:location}} />
     return children
 };
 
